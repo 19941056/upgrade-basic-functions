e6d447ea75c917666bfad8483535bc312f1ba2d4
Antonio Maestre Vargas <antoniomaestrevargasgmail.com@MBP-de-Antonio.home>
2021-10-06 21:24:07 +0200

    iteration5.nueva

--- iteration5.js
+++ iteration5.js
@@ -1,19 +1,52 @@
 const mixedElements=[6,1,'Rayo',1,'vallecano','10','upgrade',8,'hub'];
+
 function averageWord(array){
-        let sumNumb=0;
-           
-    for (let i = 0;i < array.length;i++) {
+    let suma=0;
+    for (let i = 0; i < array.length; i++) {
+        if(typeof array[i]==='string'){
+            suma+=array[i].length;
+        }else{
+            suma+=array[i];
+        }
         
-       if(typeof array[i]==='string'){
-        sumNumb+=array[i].length
-       }else{
-           sumNumb+=array[i];
-       }
-       
     }
-        console.log(sumNumb);
-        
+    console.log(suma)
 }
 averageWord(mixedElements);
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function averageWord(array){
+    let sumNumb=0;
+       
+for (let i = 0;i < array.length;i++) {
+    
+   if(typeof array[i]==='string'){
+    sumNumb+=array[i].length
+   }else{
+       sumNumb+=array[i];
+   }
+   
+}
+    console.log(sumNumb);
+    
+}
+averageWord(mixedElements);
+
+*/
